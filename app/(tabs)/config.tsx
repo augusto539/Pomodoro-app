f455859629ea1606193654b7f05a49160708dc60
augusto <augustoantonelli88@gmail.com>
2025-07-13 23:39:05 -0300

    first commit

--- app/(tabs)/config.tsx
+++ app/(tabs)/config.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Config() {
   return (
     <View
       style={{
@@ -9,8 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text> Timer screen </Text>
-      <Text> Timer screen </Text>
+      <Text> Config screen </Text>
     </View>
   );
 }
